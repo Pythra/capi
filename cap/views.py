11b824fe7745f93faf87e9a92a872aeffd2a83fd
Pythra <peze336@gmail.com>
2021-07-27 16:59:29 +0100

    yes

--- cap/views.py
+++ cap/views.py
@@ -12,7 +12,8 @@ from django.urls import reverse, reverse_lazy
 from django.utils.encoding import force_text, force_bytes
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.views.generic import View, UpdateView, DeleteView
-
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 from .forms import UserRegisterForm, AnnouncementForm, PostForm, ProfileForm
 from .models import Profile, Announcement, Comment, Reply, Post, Deposit, Wallet, Plans
 from .tokens import account_activation_token
@@ -64,7 +65,7 @@ def index(request):
     context = {'ann': ann, 'posts': posts, 'coins': coins, 'plans':plans }
     return render(request, 'cap/index.html', context)
 
-
+@method_decorator(crsf_exempt, name='dispatch')
 class SignUpView(View):
     form_class = UserRegisterForm
     template_name = 'cap/signup.html'
